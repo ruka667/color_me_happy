ea7ffad773edfc7f7bb1626903d3c9f595e68f0f
unknown <rk.0303.gun@gmail.com>
2020-10-17 17:17:15 +0900

    htmi修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,10 +27,10 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.public_file_server.enabled = true
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
 end
+
+
